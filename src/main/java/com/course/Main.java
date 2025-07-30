7a2efee8b87eab6d6cf50a5d43c8d76a696b5e67
betoalmeida83 <roberto.almeida5@fatec.sp.gov.br>
2025-07-30 18:55:27 -0300

    Initial Commit

--- src/main/java/com/course/Main.java
+++ src/main/java/com/course/Main.java
@@ -1,7 +1,36 @@
 package com.course;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        List<String> list = new ArrayList<>();
+        String path = "C:\\Users\\betog\\OneDrive\\Área de Trabalho\\in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            String name = br.readLine();
+            while (name != null) {
+                list.add(name);
+                name = br.readLine();
+            }
+            Collections.sort(list);
+            for (String s : list) {
+                System.out.println(s);
+            }
+
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
     }
+
 }
